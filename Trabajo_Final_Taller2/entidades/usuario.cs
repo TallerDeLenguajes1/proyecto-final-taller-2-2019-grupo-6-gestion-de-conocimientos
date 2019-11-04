0bc1cbaa8b8df6ed45db9182991e6ef4d1f9cc93
soyrandom1 <ignacionaval52@gmail.com>
2019-11-04 10:45:06 -0300

    clase usuario

---
+++ Trabajo_Final_Taller2/entidades/usuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace entidades
+{
+    public abstract class usuario
+    {
+        string apellido { get; set; }
+        string nombre { get; set; }
+        string pais { get; set; }
+        string email { get; set; }
+        string password { get; set; }
+        bool suspendido { get; set; }
+
+        public void verNotificacion()
+        {
+        
+        }
+        public void crearPregunta(string titulo, string descripcion, string url_imagen)
+        {
+            
+        }
+        public void crearPregunta(string titulo, string descripcion)
+        {
+
+        }
+        public void crearRespuesta(/*pregunta,*/string titulo, string drescipcion, string url_imagen)
+        {
+
+        }
+        public void crearRespuesta(/*pregunta,*/string titulo, string drescipcion)
+        {
+
+        }
+        public void agregarNotificacion(/*notificacion*/)
+        {
+
+        }
+        public void getNotificacion()
+        {
+
+        }
+        public void borrarNotificacion(/*notificacion*/)
+        {
+
+        }
+        public void darLike(/*pregunta*/)
+        {
+
+        }
+        public void banear()
+        {
+
+        }
+    }
+}
