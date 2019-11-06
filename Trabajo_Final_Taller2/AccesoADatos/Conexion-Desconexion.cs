9bd1c2cc352d48187a8960d1c682f0858ef9b050
Alejandro <ale_arc2015@hotmail.com>
2019-11-05 22:23:10 -0300

    Redone

---
+++ Trabajo_Final_Taller2/AccesoADatos/Conexion-Desconexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoADatos
+{
+    class Conexion_Desconexion
+    {
+
+        static readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=nombreDB;"
+     + "Integrated Security=true";
+        static readonly SqlConnection con = new SqlConnection(connectionString);
+
+        public static SqlConnection Con
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+        public static bool Connection()
+        {
+            try
+            {
+                Con.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static bool Desconnect()
+        {
+            try
+            {
+                Con.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+    }
+}
