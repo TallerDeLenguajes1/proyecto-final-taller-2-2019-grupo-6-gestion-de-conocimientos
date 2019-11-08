4d324cf5bd42c8bb142c9a7737bca580f8a65baf
AngelV1107 <angelleandro@live.com>
2019-11-07 21:29:49 -0300

    Movidos Clase Pregunta y Respuesta a archivos individuales

--- Trabajo_Final_Taller2/Entidades/Pregunta.cs
+++ Trabajo_Final_Taller2/Entidades/Pregunta.cs
@@ -71,34 +71,4 @@ namespace Entidades
 
         }
     }
-    public class Respuesta
-    {
-        public int IdRespuesta { get; set; }
-        public string Titulo { get; set; }
-        public string Descripcion { get; set; }
-        public string UrlImagen { get; set; }
-        public DateTime Fecha { get; set; }
-        public int Likes { get; set; }
-
-        public int GetLikes()
-        {
-            return 0;
-        }
-        public void GenerarNotificacion()
-        {
-
-        }
-        public Pregunta GetPregunta()
-        {
-
-        }
-        public Usuario GetUserResp()
-        {
-
-        }
-        public int GetIdRespuesta()
-        {
-
-        }
-    }
 }
---
+++ Trabajo_Final_Taller2/Entidades/Respuesta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Respuesta
+    {
+        public int IdRespuesta { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Likes { get; set; }
+
+        public int GetLikes()
+        {
+            return 0;
+        }
+        public void GenerarNotificacion()
+        {
+
+        }
+        public Pregunta GetPregunta()
+        {
+
+        }
+        public Usuario GetUserResp()
+        {
+
+        }
+        public int GetIdRespuesta()
+        {
+
+        }
+    }
+}
